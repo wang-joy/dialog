3fcdffd9be4907a8f8ad53b522b3f0db3446f3ab
wang-joy <wang_joy13@163.com>
2017-04-06 15:56:28 +0800

    改变目录

--- js/dialog.js
+++ js/dialog.js
@@ -17,7 +17,7 @@ Dialog.include({
         $("<div id='" + id + "' class='dialog'></div>").appendTo(body);
         var dialog = $("#" + id);
         //添加头
-        $("<div class='title'> <a href='javascript:void(0)'>标题</a> <img src='img/cancel.png' id='close'></div>").appendTo(dialog);
+        $("<div class='title'> <a href='javascript:void(0)'>标题</a> <img src='../img/cancel.png' id='close'></div>").appendTo(dialog);
         //设置内容
         $("<div class='content'></div>").appendTo(dialog);
         //设置底部
